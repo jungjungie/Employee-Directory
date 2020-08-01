464cf0617878ef894e161682864c021ca8f4c181
Esther Min <estherjmin@gmail.com>
2020-08-01 19:21:00 -0400

    added header component

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from "./components/Header/Header";
 import EmployeeTable from './components/EmployeeTable/EmployeeTable';
 import './App.css';
 
 function App() {
   return (
     <>
+      <Header />
       <EmployeeTable />
     </>
   );
---
+++ employee-directory/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <>
+            <nav>
+                <div className="nav-wrapper">
+                    <h1 className="center">Employee Directory</h1>
+                </div>
+            </nav>
+        </>
+    )
+}
