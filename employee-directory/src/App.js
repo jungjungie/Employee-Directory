3ca8933dad424b3c066c95f3513f418e66859311
Esther Min <estherjmin@gmail.com>
2020-08-03 15:37:13 -0400

    changed tempData.json to populate via React state

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header/Header';
 import EmployeeTable from './components/EmployeeTable/EmployeeTable';
+import TableRows from "./components/TableRows/TableRows";
 import SearchBar from './components/SearchBar/SearchBar';
 import './index.css';
+import tempData from "../src/temp.json";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <SearchBar />
-      <EmployeeTable />
-    </>
-  );
+
+	const [employees, setEmployees] = useState(tempData);
+
+	// setEmployees(employees);
+
+	return (
+		<>
+			<Header />
+			<SearchBar />
+			<EmployeeTable>
+				{employees.map(person => <TableRows
+					id={person.id}
+					key={person.id}
+					image={person.image}
+					name={person.name}
+					phone={person.phone}
+					email={person.email}
+					DOB={person.DOB} />)}
+			</EmployeeTable>
+		</>
+	);
 }
 
 export default App;
--- employee-directory/src/components/EmployeeTable/EmployeeTable.js
+++ employee-directory/src/components/EmployeeTable/EmployeeTable.js
@@ -1,6 +1,4 @@
 import React from "react";
-import TableRows from "../TableRows/TableRows";
-import tempData from "../../temp.json";
 
 export default function EmployeeTable(props) {
     return (
@@ -18,7 +16,7 @@ export default function EmployeeTable(props) {
                         </tr>
                     </thead>
                     <tbody>
-                        {tempData.map(person => <TableRows image={person.image} name={person.name} phone={person.phone} email={person.email} DOB={person.DOB} />)}
+                        {props.children}
                     </tbody>
                 </table>
                 <div className="col s1"></div>
