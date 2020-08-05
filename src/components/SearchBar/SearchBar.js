f01b8f6ab2b5bc153fd3378729671cc563742eb8
Esther Min <estherjmin@gmail.com>
2020-08-04 20:04:08 -0400

    edited styling

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,9 +2,11 @@ import React from "react";
 
 export default function SearchBar(props) {
     return (
-        <>
+        <div id="searchContainer" className="center">
             <i className="fa fa-search" aria-hidden="true"></i>
-            <input className="center" type="text" placeholder="Search by name or phone" onChange={props.filterSearch} />
-        </>
+            <div id="searchInput">
+                <input className="center" type="text" placeholder="Search by name or phone" onChange={props.filterSearch} />
+            </div>
+        </div>
     )
 }
